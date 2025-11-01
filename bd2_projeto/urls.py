e6aafd96eec9beb0f5e1730053dc131cd4bd7300
anaalmeida04 <anaalmeida13939@gmail.com>
2025-11-01 12:26:56 +0000

    corrigido

--- bd2_projeto/urls.py
+++ bd2_projeto/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+from core import views
+
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path('ei/horarios/', views.index, name='horarios_home'  ),
     path('admin/', admin.site.urls),
 ]
+
