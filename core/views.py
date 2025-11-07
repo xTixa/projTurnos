06c62235bba2a38cf0a15634b46afbeef2f00b84
Patricia Oliveira <patriciaisabeloliveira@outlook.pt>
2025-11-07 14:00:23 +0000

    Refactor curricular plan: update route and view, create new template for displaying units by year and semester, and enhance contact page with dynamic data

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import redirect
+from .models import UnidadeCurricular, Docente, Curso
 
 def index(request):
     return render(request, "home/index.html")
@@ -10,77 +11,24 @@ def index(request):
 def ingresso(request):
     return render(request, "home/ingresso.html")
 
-def plano(request):
-    plano_curricular = [
-        {
-            "ano": 1,
-            "semestre": 1,
-            "ucs": [
-                {"nome": "Análise Matemática", "ects": 5.5, "tipo": "Obrigatória", "docente": "Cecília", "descricao": "Introdução à lógica, conjuntos e estruturas discretas."},
-                {"nome": "Álgebra", "ects": 4.5, "tipo": "Obrigatória", "docente": "Lurdes Sousa", "descricao": "Conceitos básicos de programação em C."},
-                {"nome": "Sistemas Digitais", "ects": 6, "tipo": "Obrigatória", "docente": "Francisco Francisco", "descricao": "Circuitos combinatórios e sequenciais."},
-                {"nome": "Algoritmos e Programação", "ects": 8, "tipo": "Obrigatória", "docente": "Joana Fialho", "descricao": "Circuitos combinatórios e sequenciais."},
-                {"nome": "Tecnologias dos Computadores", "ects": 6, "tipo": "Obrigatória", "docente": "Carlos Simoes", "descricao": "Circuitos combinatórios e sequenciais."},
-            ],
-        },
-        {
-            "ano": 1,
-            "semestre": 2,
-            "ucs": [
-                {"nome": "Arquiteturas de Computador", "ects": 5.5, "tipo": "Obrigatória", "docente": "Rita Pinto", "descricao": "Álgebra linear e cálculo diferencial."},
-                {"nome": "Matemática Discreta", "ects": 5, "tipo": "Obrigatória", "docente": "Pedro Santos", "descricao": "Estruturas de dados e modularidade."},
-                {"nome": "Matemática Aplicada", "ects": 5, "tipo": "Obrigatória", "docente": "Marta Dias", "descricao": "Gestão de processos e memória."},
-                {"nome": "Redes de Comunicação I", "ects": 6.5, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-                {"nome": "Estruturas de Dados", "ects": 8, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-            ],
-        },
-        {
-            "ano": 2,
-            "semestre": 1,
-            "ucs": [
-                {"nome": "Programação Orientada a Objetos", "ects": 6.5, "tipo": "Obrigatória", "docente": "Fernando Silva", "descricao": "Modelo relacional e SQL."},
-                {"nome": "Análise de Sistemas", "ects": 6.5, "tipo": "Obrigatória", "docente": "Paulo Correia", "descricao": "Requisitos, UML e metodologias ágeis."},
-                {"nome": "Sistemas Operativos", "ects": 5.5, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-                {"nome": "Aplicações para a Internet I", "ects": 6.5, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-                {"nome": "Usabilidade", "ects": 5, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-            ],
-        },
-        {
-            "ano": 2,
-            "semestre": 2,
-            "ucs": [
-                {"nome": "Engenharia de Software I", "ects": 4.5, "tipo": "Obrigatória", "docente": "Fernando Silva", "descricao": "Modelo relacional e SQL."},
-                {"nome": "Bases de Dados I", "ects": 6, "tipo": "Obrigatória", "docente": "Paulo Correia", "descricao": "Requisitos, UML e metodologias ágeis."},
-                {"nome": "Aplicações para a Internet II", "ects": 5, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-                {"nome": "Redes de Comunicação II", "ects": 4.5, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-                {"nome": "Programação para Dispositivos Móveis", "ects": 5, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-                {"nome": "Projeto Integrado", "ects": 5, "tipo": "Obrigatória", "docente": "", "descricao": "Circuitos combinatórios e sequenciais."},
-            ],
-        },
-        {
-            "ano": 3,
-            "semestre": 1,
-            "ucs": [
-                {"nome": "Segurança Informática", "ects": 4.5, "tipo": "Obrigatória", "docente": "Fernando Silva", "descricao": "Modelo relacional e SQL."},
-                {"nome": "Redes de Comunicação III", "ects": 4.5, "tipo": "Obrigatória", "docente": "Paulo Correia", "descricao": "Requisitos, UML e metodologias ágeis."},
-                {"nome": "Complementos de Sistemas Operativos", "ects": 5, "tipo": "Obrigatória", "docente": "Fernando Silva", "descricao": "Modelo relacional e SQL."},
-                {"nome": "Bases de Dados II", "ects": 6, "tipo": "Obrigatória", "docente": "Paulo Correia", "descricao": "Requisitos, UML e metodologias ágeis."},
-                {"nome": "Sistemas Embebidos", "ects": 5, "tipo": "Obrigatória", "docente": "Fernando Silva", "descricao": "Modelo relacional e SQL."},
-                {"nome": "Sistemas Distribuídos", "ects": 5, "tipo": "Obrigatória", "docente": "Paulo Correia", "descricao": "Requisitos, UML e metodologias ágeis."},
-            ],
-        },
-        {
-            "ano": 3,
-            "semestre": 2,
-            "ucs": [
-                {"nome": "Inteligência Artificial", "ects": 5, "tipo": "Obrigatória", "docente": "Fernando Silva", "descricao": "Modelo relacional e SQL."},
-                {"nome": "Engenharia de Software II", "ects": 6, "tipo": "Obrigatória", "docente": "Paulo Correia", "descricao": "Requisitos, UML e metodologias ágeis."},
-                {"nome": "Empreendedorismo e Gestão de Empresas", "ects": 3, "tipo": "Obrigatória", "docente": "Fernando Silva", "descricao": "Modelo relacional e SQL."},
-                {"nome": "Projeto", "ects": 16, "tipo": "Obrigatória", "docente": "Paulo Correia", "descricao": "Requisitos, UML e metodologias ágeis."},
-            ],
-        },
-    ]
-    return render(request, "home/plano.html", {"plano": plano_curricular})
+def plano_curricular(request):
+    # Busca todas as unidades, com joins automáticos
+    unidades = UnidadeCurricular.objects.select_related('id_anocurricular', 'id_semestre').order_by(
+        'id_anocurricular__id_anocurricular', 'id_semestre__id_semestre'
+    )
+
+    # Organiza por ano e semestre
+    plano = {}
+    for uc in unidades:
+        ano = uc.id_anocurricular.ano_curricular
+        semestre = uc.id_semestre.semestre
+        if ano not in plano:
+            plano[ano] = {}
+        if semestre not in plano[ano]:
+            plano[ano][semestre] = []
+        plano[ano][semestre].append(uc)
+
+    return render(request, "home/plano_curricular.html", {"plano": plano})
 
 def horarios(request):
     horarios = {
@@ -119,7 +67,17 @@ def avaliacoes(request):
     return render(request, "home/avaliacoes.html", {"avaliacoes_docs": avaliacoes_docs})
 
 def contactos(request):
-    return render(request, "home/contactos.html")
+    # Buscar todos os docentes
+    docentes = Docente.objects.all().order_by('nome')
+
+    # Buscar dados do curso principal (por exemplo Engenharia Informática)
+    curso = Curso.objects.filter(nome__icontains="Engenharia Informática").first()
+
+    contexto = {
+        "curso": curso,
+        "docentes": docentes
+    }
+    return render(request, "home/contactos.html", contexto)
 
 def informacoes(request):
     return render(request, "home/informacoes.html")
