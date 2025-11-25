922e402a570f59d563c1030cdecae25bb3430966
Susana Tavares <susana15tavares@hotmail.com>
2025-11-25 23:34:15 +0000

    Atualização para MongoDB Atlas

--- bd2_projeto/mongodb.py
+++ bd2_projeto/mongodb.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb://localhost:27017/"
+
+MONGO_URI = "mongodb+srv://projTurnos:projTurnos123@projturnoscluster.sjnik21.mongodb.net/?retryWrites=true&w=majority&appName=projTurnosCluster"
 
 client = MongoClient(MONGO_URI)
 
